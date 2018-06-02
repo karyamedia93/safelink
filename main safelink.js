204d0589d3a3cc706db66bec0cadab1ca6facb88
karyamedia93 <39864046+karyamedia93@users.noreply.github.com>
2018-06-03 02:29:54 +0700

    Add files via upload

---
+++ main safelink.js
@@ -0,0 +1,215 @@
+
+
+<div class="text-center margin-bottom-20">
+<h3>Advertise</h3>
+KODE IKLAN DISINI
+  </div>
+
+<div class="row margin-bottom-20">
+  <div class="col-md-6">
+<div class="panel panel-primary">
+    <div class="panel-heading text-center"><h2><i class="fa fa-shield"></i> Safe Link Converter <i class="fa fa-shield"></i></h2></div>
+        <div class="panel-body text-center">
+          <p class="text-primary margin-bottom-20">Encrypting your link and protect the link from viruses, malware, thief, etc!<br/>Made your link safe to visit.</p>
+
+<div class="progress" id="daplong">
+    <div class="progress-bar progress-bar-striped active six-sec-ease-in-out" role="progressbar" data-transitiongoal="100"></div>
+</div>
+<button id="download2" class="alert alert-dismissible alert-success btn-lg" style='display:none'><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i class="fa fa-hand-o-down slideInDownThumb" aria-hidden="true"></i></button>
+
+        </div>
+      </div>
+</div>
+  <div class="col-md-6 text-left">
+<div class="panel panel-info">
+  <!-- Default panel contents -->
+  <div class="panel-heading"><h2><i class="fa fa-question-circle-o" aria-hidden="true"></i> How to use our tool:</h2></div>
+  <div class="panel-body">
+<ol>
+<li>Click on <strong>How To Use</strong> menu above.</li>
+<li>Click on the code and <kbd><kbd>CTRL</kbd> + <kbd>C</kbd></kbd> on your keyboard.</li>
+<li>Paste the code in your HTML blog theme before the <mark>&lt;/body&gt;</mark>.</li>
+<li>Save your HTML blog theme. you are done!</li>
+<li>Now, your blog's outbound links was encrypted!</li>
+</ol>
+  </div>
+</div>
+</div><div class="clear"></div>
+</div>
+
+<div class="row margin-bottom-20">
+  <div class="col-md-4 text-center">
+<h3>Advertise</h3>
+KODE IKLAN DISINI
+</div>
+  <div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h3 class="panel-title">
+<b>Your link show here</b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
+</div>
+<div class="panel-body">
+<script src="https://cdn.rawgit.com/dedi96/safelink/813ce72f/safelinkga.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://karyamediakita.blogspot.com/p/generate.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+ <div class="col-md-4 text-center">
+<h3>Advertise</h3>
+KODE IKLAN DISINI
+</div>
+<div class="clear"></div>
+</div>
+
+<div class="container">
+  <div class="text-left">
+
+  </div>
+</div>
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+<div class="text-center margin-bottom-20">
+<h3>Advertise</h3>
+KODE IKLAN DISINI
+  </div>
+ 
+<div class="row margin-bottom-20">
+  <div class="col-md-6">
+<div class="panel panel-primary">
+    <div class="panel-heading text-center"><h2><i class="fa fa-shield"></i> Safe Link Converter <i class="fa fa-shield"></i></h2></div>
+        <div class="panel-body text-center">
+          <p class="text-primary margin-bottom-20">Encrypting your link and protect the link from viruses, malware, thief, etc!<br/>Made your link safe to visit.</p>
+ 
+<div class="progress" id="daplong">
+    <div class="progress-bar progress-bar-striped active six-sec-ease-in-out" role="progressbar" data-transitiongoal="100"></div>
+</div>
+<button id="download2" class="alert alert-dismissible alert-success btn-lg" style='display:none'><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> <strong>Well done!</strong> you have successfully gained access to Decrypted Link. <i class="fa fa-hand-o-down slideInDownThumb" aria-hidden="true"></i></button>
+ 
+        </div>
+      </div>
+</div>
+  <div class="col-md-6 text-left">
+<div class="panel panel-info">
+  <!-- Default panel contents -->
+  <div class="panel-heading"><h2><i class="fa fa-question-circle-o" aria-hidden="true"></i> How to use our tool:</h2></div>
+  <div class="panel-body">
+<ol>
+<li>Click on <strong>How To Use</strong> menu above.</li>
+<li>Click on the code and <kbd><kbd>CTRL</kbd> + <kbd>C</kbd></kbd> on your keyboard.</li>
+<li>Paste the code in your HTML blog theme before the <mark>&lt;/body&gt;</mark>.</li>
+<li>Save your HTML blog theme. you are done!</li>
+<li>Now, your blog's outbound links was encrypted!</li>
+</ol>
+  </div>
+</div>
+</div><div class="clear"></div>
+</div>
+ 
+<div class="row margin-bottom-20">
+  <div class="col-md-4 text-center">
+<h3>Advertise</h3>
+KODE IKLAN DISINI
+</div>
+  <div class="col-md-4 margin-top-25">
+<div class="panel panel-success text-center">
+<div class="panel-heading">
+<h3 class="panel-title">
+<b>Your link show here</b> <i aria-hidden="true" class="fa fa-hand-o-down"></i></h3>
+</div>
+<div class="panel-body">
+<script src="https://cdn.rawgit.com/dedi96/safelink/813ce72f/safelinkga.js" type="text/javascript"></script>
+<script type="text/javascript">
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://alamat-url.blogspot.com/p/generate.html?url=", "");
+document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
+</script>
+  </div>
+</div>
+</div>
+ <div class="col-md-4 text-center">
+<h3>Advertise</h3>
+KODE IKLAN DISINI
+</div>
+<div class="clear"></div>
+</div>
+ 
+<div class="container">
+  <div class="text-left">
+ 
+  </div>
+</div>
