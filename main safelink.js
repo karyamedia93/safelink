509b03cd2f002a6e5d69b4fdf59b0a827e54fece
karyamedia93 <39864046+karyamedia93@users.noreply.github.com>
2018-06-04 01:24:49 +0700

    Update main safelink.js

--- main safelink.js
+++ main safelink.js
@@ -53,7 +53,7 @@ KODE IKLAN DISINI
 <script type="text/javascript">
 var currentURL=location.href;
 var str = currentURL;
-var res = str.replace("http://karyamediakita.blogspot.com/p/generate.html?url=", "");
+var res = str.replace("https://karyamediakita.blogspot.com/p/generate.html?url=", "");
 document.write('<button type="button" id="download" class="Visit_Link btn btn-success" onclick="changeLink();" style="display: none;"><strong>Visit Link</strong> <i class="fa fa-external-link" aria-hidden="true"></i></button>')
 </script>
   </div>
